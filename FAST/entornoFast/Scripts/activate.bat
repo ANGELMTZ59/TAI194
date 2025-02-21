e44a31eb5abd8c30a47b75297ba2ddfb7440c802
ANGELMTZ59 <38768058+ANGELMTZ59@users.noreply.github.com>
2025-02-21 15:22:07 -0600

    pydantic

--- FAST/entornoFast/Scripts/activate.bat
+++ FAST/entornoFast/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\100097567\Desktop\TAI194\entornoFast
+set VIRTUAL_ENV=C:\Users\100097567\Desktop\TAI194\fast\entornoFast
 
 if not defined PROMPT set PROMPT=$P$G
 
--- FAST/main.py
+++ FAST/main.py
@@ -1,15 +1,23 @@
 from fastapi import FastAPI, HTTPException
-from typing import Optional # define para que los caracteres en las api sean opcionales o no
+from typing import Optional, List # define para que los caracteres en las api sean opcionales o no
+from pydantic import BaseModel
+
 app = FastAPI(
     title="Mi primera API",
     description="Angel Daniel Martinez Maqueda",
     version="1.0.1"
 )
+class modelUsuario(BaseModel):
+    id: int
+    nombre: str
+    edad: int
+    correo: str
+
 usuarios=[
-    {"id":1, "nombre":"Angel", "edad":20},
-    {"id":2, "nombre":"Daniel", "edad":20},
-    {"id":3, "nombre":"Alfredo", "edad":21},
-    {"id":4, "nombre":"Aaron", "edad":22},
+    {"id":1, "nombre":"Angel", "edad":20,"correo":"angelmaryinez1@gmail.com"},
+    {"id":2, "nombre":"Daniel", "edad":20,"correo":"daniel@gmail.com"},
+    {"id":3, "nombre":"Alfredo", "edad":21,"correo":"alfredo@gmail.com"},
+    {"id":4, "nombre":"Aaron", "edad":22,"correo":"aaron@gmail.com"},
 ]
 
 @app.get("/", tags=['Inicio'])
@@ -19,26 +27,26 @@ def main():
 
 
 # Enpoint CONSULTA TODOS
-@app.get("/todosUsuarios/", tags=['Operaciones CRUD'])
+@app.get("/todosUsuarios/", response_model=List[modelUsuario], tags=['Operaciones CRUD'])
 def leer():
-    return { 'Usuarios Registrados: ': usuarios}
+    return usuarios
 
 
 #Endpoint de tipo POST
-@app.post("/usuarios/", tags=['Operaciones CRUD'])
-def guardar(usuario:dict):
+@app.post("/usuarios/", response_model= modelUsuario, tags=['Operaciones CRUD'])
+def guardar(usuario:modelUsuario):
     for usr in usuarios:
-        if usr["id"]==usuario.get("id"):
+        if usr["id"]==usuario.id:
          raise HTTPException(status_code=400, detail="El usuario ya existe")
-    usuarios.append(usuario)
+    usuarios.append(usuario.dict())
     return usuario
 
 #Endpoint para actualizar
-@app.put("/usuarios/{id}", tags=['Operaciones CRUD'])
-def actualizar(id:int, usuarioActualizado:dict):
+@app.put("/usuarios/{id}",response_model=modelUsuario, tags=['Operaciones CRUD'])
+def actualizar(id:int, usuarioActualizado: modelUsuario):
     for index, usr in enumerate(usuarios):
         if usr["id"]==id:
-            usuarios[index].update(usuarioActualizado)
+            usuarios[index]=usuarioActualizado.model_dump()
             return usuarios[index]
     raise HTTPException(status_code=400, detail="El usuario no existe")
 
