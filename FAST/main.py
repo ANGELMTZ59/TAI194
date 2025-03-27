7241c78923e1abf0d3121f78528523f0d0496b9f
ANGELMTZ59 <38768058+ANGELMTZ59@users.noreply.github.com>
2025-03-27 15:00:45 -0600

    base de datos

--- FAST/main.py
+++ FAST/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from typing import List
 from modelsPydantics import modelUsuario, modelAuth
@@ -15,6 +16,7 @@ app = FastAPI(
 )
 
 Base.metadata.create_all(bind=engine)
+
 
 usuarios=[
     {"id":1, "nombre":"Angel", "edad":20,"correo":"angelmaryinez1@gmail.com"},
@@ -47,10 +49,17 @@ def auth(credenciales:modelAuth):
 
 
 # Enpoint CONSULTA TODOS
-@app.get("/todosUsuarios/", response_model=List[modelUsuario], tags=['Operaciones CRUD'])
-def leer(token: str = Depends(BearerJWT())):
-    print("Token:", token)
-    return usuarios
+@app.get("/todosUsuarios", tags=['Operaciones CRUD'])
+def leer():
+    db=Session()
+    try:
+        consulta= db.query(User).all()
+        return JSONResponse( content=jsonable_encoder(consulta))
+    except Exception as e:
+        db.rollback()
+        return JSONResponse(status_code=500, content={"message": "Error al guardar", "Error": str(e)})
+    finally:
+        db.close()
 
 
 
@@ -71,25 +80,69 @@ def guardar(usuario: modelUsuario):
 
 
 
+#Endpoint para buscar por id
+@app.get("/usuario/{id}", tags=['Operaciones CRUD'])
+def leeruno(id:int):
+    db=Session()
+    try:
+        consulta1= db.query(User).filter(User.id == id).first()
+
+        if not consulta1:
+            return JSONResponse(status_code=404, content={"message": "Usuario no encontrado"})
+        
+        return JSONResponse( content=jsonable_encoder(consulta1))
+    except Exception as e:
+        db.rollback()
+        return JSONResponse(status_code=500, content={"message": "Error al guardar", "Error": str(e)})
+    finally:
+        db.close()
+
+
+
+
 #Endpoint para actualizar
-@app.put("/usuarios/{id}",response_model=modelUsuario, tags=['Operaciones CRUD'])
-def actualizar(id:int, usuarioActualizado: modelUsuario):
-    for index, usr in enumerate(usuarios):
-        if usr["id"]==id:
-            usuarios[index]=usuarioActualizado.model_dump()
-            return usuarios[index]
-    raise HTTPException(status_code=400, detail="El usuario no existe")
+@app.put("/usuarios/{id}", response_model=modelUsuario, tags=['Operaciones CRUD'])
+def actualizar(id: int, usuarioActualizado: modelUsuario):
+    db = Session()
+    try:
+        query = db.query(User).filter(User.id == id)
+        if not query.first():
+            return JSONResponse(status_code=404, content={"message": "Usuario no encontrado"})
+        
+        actualizar_info = usuarioActualizado.model_dump(exclude_unset=True)
+        query.update(actualizar_info)
+        db.commit()
+        usuario_actualizado = db.query(User).filter(User.id == id).first()
+
+
+
+
+        return JSONResponse(content=jsonable_encoder(usuario_actualizado))
+    except Exception as e:
+        db.rollback()
+        return JSONResponse(status_code=500, content={"message": "Error al actualizar", "Error": str(e)})
+    finally:
+        db.close()
+
 
 
 
 #Endpoint para eliminar
 @app.delete("/usuarios/{id}", tags=['Operaciones CRUD'])
 def eliminar(id:int):
-    for index, usr in enumerate(usuarios):
-        if usr["id"]==id:
-            usuarios.pop(index)
-            return { 'Usuarios Registrados: ': usuarios}
-    raise HTTPException(status_code=400, detail="El usuario no existe")
-
-
+    db = Session()
+    try:
+        query = db.query(User).filter(User.id == id)
+        usuario_eliminado = query.first()
+        if not usuario_eliminado:
+            return JSONResponse(status_code=404, content={"message": "Usuario no encontrado"})
+        db.delete(usuario_eliminado)
+        db.commit()
+        
+        return JSONResponse(content=jsonable_encoder(usuario_eliminado))
+    except Exception as e:
+        db.rollback()
+        return JSONResponse(status_code=500, content={"message": "Error al eliminar", "Error": str(e)})
+    finally:
+        db.close()
 
